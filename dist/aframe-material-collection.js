8c60d5867143902912244efd8f259570669ea6d4
arpu <freen.mail@yandex.com>
2019-03-21 02:45:39 +0100

    add slider background and set the width and height

--- dist/aframe-material-collection.js
+++ dist/aframe-material-collection.js
@@ -1134,13 +1134,13 @@ module.exports = AFRAME.registerComponent('ui-slider', {
         // Setup background with mouse input to catch mouse move events when not exactly over the scroll bar.
         this.backgroundPanel = document.createElement('a-plane');
         this.backgroundPanel.setAttribute('class','no-yoga-layout background '+this.data.intersectableClass);
-        this.backgroundPanel.setAttribute('width',this.data.width+0.1);
-        this.backgroundPanel.setAttribute('height',this.data.height+0.1);
+        this.backgroundPanel.setAttribute('width',this.data.width+0.01);
+        this.backgroundPanel.setAttribute('height',this.data.height+0.01);
         this.backgroundPanel.setAttribute('position','0 0 -0.3');
         this.backgroundPanel.setAttribute('opacity',0.0001);//
         this.backgroundPanel.setAttribute('transparent',true);
           
-        //this.el.appendChild(this.backgroundPanel);
+        this.el.appendChild(this.backgroundPanel);
         // Setup handle circle entity.
         this.handleEl = document.createElement('a-circle');
         this.handleEl.setAttribute('radius',this.data.handleRadius);
