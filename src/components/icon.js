574aa998a2d08393a63fa4ccdba5e85e1ad83567
arpu <arnold.putz@gmail.com>
2021-04-13 16:39:16 +0200

    update

--- src/components/icon.js
+++ src/components/icon.js
@@ -9,8 +9,8 @@ module.exports = AFRAME.registerComponent( "ui-icon", {
 	schema: {
 		src: { default: "" },
 		size: { type: "vec2", default: { x: 0.1, y: 0.1 } },
-		width:  { type: "number", default: 0.1 },
-		height:  { type: "number", default: 0.1 },
+		width: { type: "number", default: 0.1 },
+		height: { type: "number", default: 0.1 },
 		zIndex: { type: "number", default: 0.0001 },
 		color: { default: "#fff" },
 		iconmesh: { default: "circle" },
@@ -19,53 +19,37 @@ module.exports = AFRAME.registerComponent( "ui-icon", {
 	},
 	init() {
 
-		//this._manager = new THREE.LoadingManager();
-        //this._loader = new THREE.TextureLoader(this._manager);
+		
+		this._loader = new THREE.TextureLoader(  );
 
 	},
 
 	update() {
 
-const Mesh = this.el.object3D.getObjectByProperty( "type", "Mesh" )
-
-
-
-
+		const Mesh = this.el.object3D.getObjectByProperty( "type", "Mesh" );
 
 		//this.el.setAttribute( "a-image", "src", this.data.src );
-		const textureMap  =  new THREE.TextureLoader().load( this.data.src );
+		const textureMap = this._loader.load( this.data.src );
 		let width, height;
-        if (Mesh.geometry.boundingBox)  {
+		if ( Mesh.geometry.boundingBox ) {
 
-		   width = Mesh.geometry.boundingBox.max.x
-			height = Mesh.geometry.boundingBox.max.y
-		}
+		   width = Mesh.geometry.boundingBox.max.x;
+			height = Mesh.geometry.boundingBox.max.y;
 
-		else {
+		} else {
+
 			width = this.data.width;
 			height = this.data.height;
 
 		}
-         
-
-
-
 
 		//const material = new THREE.MeshBasicMaterial( { emissive:this.data.color, alphaTest: 0.4, transparent: false, map: textureMap } );
-		
-		const material = new THREE.MeshBasicMaterial({ transparent: true, map: textureMap } );
-		
-		material.onUpdate = function () {
 
-			// Delete texture data once it has been uploaded to the GPU
-
-			material.mipmaps.length = 0;
-
-		};
+		const material = new THREE.MeshBasicMaterial( { transparent:true, map: textureMap } );
 
 		if ( this.data.iconmesh == "circle" )
 			this.icon = new THREE.Mesh(
-				new THREE.CircleGeometry( this.data.radius , 32 ),
+				new THREE.CircleGeometry( this.data.radius, 32 ),
 				material,
 			);
 		else
@@ -74,19 +58,19 @@ const Mesh = this.el.object3D.getObjectByProperty( "type", "Mesh" )
 				material,
 			);
 
-
 		this.icon.position.set( 0, 0, this.data.zIndex );
 		this.el.object3D.add( this.icon );
 
 	},
 	remove() {
-
+        console.info("icon remove()")
 		this.el.object3D.remove( this.el.object3D );
-	
-		this.icon.geometry.dispose();
+
+		/*this.icon.geometry.dispose();
 		this.icon.material.map.dispose();
 		this.icon.material.dispose();
-	    this.icon = null;
+	    */
+		this.icon = null;
 		this.el.remove( "ui-icon" );
 
 	},
